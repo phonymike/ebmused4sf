8191de96dba8ed819899f2c04da7ca4265599543
phonymike <99826918+phonymike@users.noreply.github.com>
2022-08-16 21:06:15 -0400

    initial commit

---
+++ src/versioninfo.h
@@ -0,0 +1,4 @@
+#define VER_FILEVERSION				0,5,1,0
+#define VER_FILEVERSION_STR			"0,5,1,0"
+#define CLI_FILEVERSION_STR			"v0.5.1a"
+#define CLI_FILEDESCRIPTION_STR		"Earthbound Music Editor (but for StarFox)"
